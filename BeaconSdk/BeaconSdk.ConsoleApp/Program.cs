86015b7ac6e8fa6f34e036837e0be07c0ba483c6
iviich4eL <Mich4el@protonmail.com>
2021-04-12 19:28:53 +0300

    Create ConsoleApp

---
+++ BeaconSdk/BeaconSdk.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeaconSdk.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
