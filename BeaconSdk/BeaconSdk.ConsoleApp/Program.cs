f3d752c863f789c8afe486226b4c72388fa7d867
Mich4el <Mich4el@protonmail.com>
2021-04-20 02:02:01 +0300

    Add MatrixSync

--- BeaconSdk/BeaconSdk.ConsoleApp/Program.cs
+++ BeaconSdk/BeaconSdk.ConsoleApp/Program.cs
@@ -18,17 +18,22 @@
 
             var matrixUserService = serviceProvider.GetService<MatrixUserService>();
             var matrixRoomService = serviceProvider.GetService<MatrixRoomService>();
+            var matrixEventService = serviceProvider.GetService<MatrixEventService>();
 
             var seed = Guid.NewGuid().ToString(); //Todo: generate once and then store seed?
-
             var responseLogin = await matrixUserService!.LoginAsync(seed);
-            var responseCreateRoom = await matrixRoomService!.CreateRoomAsync(responseLogin.AccessToken, "");
-            var responseJoinedRooms = await matrixRoomService.GetJoinedRoomsAsync(responseLogin.AccessToken);
+
+            var accessToken = responseLogin.AccessToken;
+
+            var responseCreateRoom = await matrixRoomService!.CreateRoomAsync(accessToken, "");
+            var responseJoinedRooms = await matrixRoomService.GetJoinedRoomsAsync(accessToken);
 
             Console.WriteLine($"RoomId: {responseCreateRoom.RoomId}");
 
             foreach (var room in responseJoinedRooms.JoinedRooms)
                 Console.WriteLine($"Joined room: {room}");
+
+            var t = await matrixEventService!.SyncAsync(accessToken);
         }
     }
 }
--- BeaconSdk/MatrixSdk/Services/MatrixEventService.cs
+++ BeaconSdk/MatrixSdk/Services/MatrixEventService.cs
@@ -2,9 +2,12 @@
 {
     using System.Net.Http;
     using System.Threading.Tasks;
+    using Dto;
+    using Extensions;
 
     public class MatrixEventService
     {
+        private const string RequestUri = "_matrix/client/r0";
         private readonly IHttpClientFactory httpClientFactory;
 
         public MatrixEventService(IHttpClientFactory httpClientFactory)
@@ -12,10 +15,19 @@
             this.httpClientFactory = httpClientFactory;
         }
 
-        public async Task<int> SyncAsync()
+        public async Task<MatrixSyncResponse> SyncAsync(string accessToken)
         {
-            
-            return await Task.FromResult(42);
+            var httpClient = httpClientFactory.CreateClient(Constants.Matrix);
+            httpClient.AddBearerToken(accessToken);
+
+            return await httpClient.GetAsJsonAsync<MatrixSyncResponse>($"{RequestUri}/sync");
+
+            // var t = new MatrixSyncResponse();
+            // // t.MatrixRooms.Invite[0].InviteState.Events[0].
+            // // var k = t.Rooms.Join[0].TimeLine.Events[0].Sender;
+            // // Dto.Sync.Rooms a = t.Rooms;
+            // // Dto.Sync.Rooms? b = t.Rooms;
+            // return await Task.FromResult(42);
         }
     }
 }
