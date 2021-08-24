a056f51fdf67bff86fad8590dfc668c099e92944
iviich4eL <Mich4el@protonmail.com>
2021-08-24 17:40:11 +0300

    cleanup

--- BeaconSdk/MatrixSdk/Infrastructure/Providers/LibsodiumAlgorithmsProvider.cs
+++ BeaconSdk/MatrixSdk/Infrastructure/Providers/LibsodiumAlgorithmsProvider.cs
@@ -6,7 +6,7 @@ namespace MatrixSdk.Infrastructure.Providers
     {
         public byte[] GenerateRandomBytes(int count) => SodiumCore.GetRandomBytes(count);
 
-        public byte[] Hash(string message, int size) => GenericHash.Hash(message, (byte[]?) null, size);
+        public byte[] Hash(string message, int size) => GenericHash.Hash(message, (byte[]?)null, size);
 
         public byte[] Hash(byte[] message, int size) => GenericHash.Hash(message, null, size);
 
