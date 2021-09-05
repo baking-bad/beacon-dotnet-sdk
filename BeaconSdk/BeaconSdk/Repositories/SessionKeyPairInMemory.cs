946ac1ece352f0ea1adf05ed3fb6e559783b6c05
iviich4eL <Mich4el@protonmail.com>
2021-09-05 22:49:06 +0300

    Create SessionKeyPairInMemory storage

---
+++ BeaconSdk/BeaconSdk/Repositories/SessionKeyPairInMemory.cs
@@ -0,0 +1,27 @@
+namespace BeaconSdk.Repositories
+{
+    using System.Collections.Generic;
+    using Libsodium;
+    using MatrixSdk.Utils;
+    using Sodium;
+
+    public class SessionKeyPairInMemory
+    {
+        private static readonly Dictionary<HexString, KeyPair> clientSessionKeyPairs = new();
+        private static readonly Dictionary<HexString, SessionKeyPair> ServerSessionKeyPairs = new();
+
+        public static SessionKeyPair CreateOrReadServer(HexString clientPublicKey, KeyPair serverKeyPair) =>
+            ServerSessionKeyPairs.TryGetValue(clientPublicKey, out var sessionKeyPair)
+                ? sessionKeyPair
+                : CreateServerSessionKeyPair(clientPublicKey, serverKeyPair);
+        
+        private static SessionKeyPair CreateServerSessionKeyPair(HexString clientPublicKey, KeyPair serverKeyPair)
+        {
+            var sessionKeyPair = EncryptionService.CreateServerSessionKeyPair(clientPublicKey.ToByteArray(), serverKeyPair.PrivateKey);
+
+            ServerSessionKeyPairs[clientPublicKey] = sessionKeyPair;
+
+            return sessionKeyPair;
+        }
+    }
+}
