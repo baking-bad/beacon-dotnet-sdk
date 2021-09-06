30f0a2f81d793089fb2611e095212209b4375cf8
iviich4eL <Mich4el@protonmail.com>
2021-09-06 21:38:30 +0300

    Update Program.cs

--- BeaconSdk/BeaconSdk.ConsoleApp/Program.cs
+++ BeaconSdk/BeaconSdk.ConsoleApp/Program.cs
@@ -1,19 +1,17 @@
 ﻿namespace BeaconSdk.ConsoleApp
 {
     using System;
-    using System.IO;
-    using System.Reflection.Metadata;
     using System.Threading;
     using System.Threading.Tasks;
-    using MatrixSdk;
+    using Domain.Pairing;
+    using Infrastructure.Cryptography.Libsodium;
     using MatrixSdk.Application;
     using MatrixSdk.Application.Listener;
-    using MatrixSdk.Infrastructure.Providers;
     using MatrixSdk.Infrastructure.Services;
-    using MatrixSdk.Utils;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
     using Serilog;
 
     internal class Program
@@ -28,33 +26,64 @@
         private static async Task<int> Main(string[] args)
         {
             var host = CreateHostBuilder().Build();
-            
+
             var theme = LoggerSetup.SetupTheme();
             Log.Logger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
                 .WriteTo.Console(theme: theme)
                 .CreateLogger();
-            
+
             try
             {
                 var logger = host.Services.GetRequiredService<ILogger<Program>>();
-            
+
                 logger.LogInformation("START");
-            
+
                 await RunAsync(host.Services);
             }
             catch (Exception ex)
             {
                 var logger = host.Services.GetRequiredService<ILogger<Program>>();
-            
-                logger.LogError(ex, "An error occurred.");
+
+                logger.LogError(ex, "An error occurred");
             }
 
             return 0;
         }
 
-        private static async Task RunAsync(IServiceProvider serviceProvider)
-        {
+        // private static async Task RunAsync(IServiceProvider serviceProvider)
+        // {
+            // var pairingResponse = new PairingResponse(
+            //     "1", 
+            //     "p2p-pairing-response",
+            //     "101112",
+            //     "789",
+            //     "456", 
+            //     "123");
+
+            // var pairingResponse = new PairingResponse(
+            //     null,
+            //     "p2p-pairing-response",
+            //     null,
+            //     null,
+            //     null,
+            //     null);
+            //
+            // var t = JsonConvert.SerializeObject(pairingResponse);
+            // if (t == "null") t = " empty";
+            // Console.WriteLine(t);
+
+            // var numbers = new []{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            // foreach (var number in numbers)
+            //     Console.Write($"{number} ");
+            // Console.WriteLine();
+            //
+            // foreach (var i in numbers[1..5])
+            //     Console.Write($"{i} ");
+            // Console.WriteLine();
+
+            // EncryptionService.DecryptWithSharedKey(null, null);
+
             // var a1 = "0xFAFA";
             // if (HexString.TryParse(a1, out var r1)) 
             //     Console.WriteLine(r1);
@@ -72,8 +101,7 @@
             //
             // var b3 = r3.ToByteArray();
             // Console.WriteLine(r3.ToASCII());
-            
-           
+
 
             // var a3 = "48656c6c6f20576f726c6421";
             // if (HexString.TryParse(a3, out var r3)) 
@@ -88,62 +116,64 @@
 
             // Object a = new Object();
             // var t = (HexString)a;
-
-        }
-        
-        // private static async Task RunAsync(IServiceProvider serviceProvider)
-        // {
-        //     var (firstClient, firstListener) = await SetupClientWithTextListener(serviceProvider);
-        //     var (secondClient, secondListener) = await SetupClientWithTextListener(serviceProvider);
-        //
-        //     var firstClientMatrixRoom = await firstClient.CreateTrustedPrivateRoomAsync(new[]
-        //     {
-        //         secondClient.UserId
-        //     });
-        //
-        //     var matrixRoom = await secondClient.JoinTrustedPrivateRoomAsync(firstClientMatrixRoom.Id);
-        //
-        //     var spin = new SpinWait();
-        //     while (secondClient.JoinedRooms.Length == 0)
-        //         spin.SpinOnce();
-        //
-        //     await firstClient.SendMessageAsync(firstClientMatrixRoom.Id, "Hello");
-        //     await secondClient.SendMessageAsync(secondClient.JoinedRooms[0].Id, ", ");
-        //
-        //     await firstClient.SendMessageAsync(firstClientMatrixRoom.Id, "World");
-        //     await secondClient.SendMessageAsync(secondClient.JoinedRooms[0].Id, "!");
-        //
-        //     Console.ReadLine();
-        //
-        //     firstClient.Stop();
-        //     secondClient.Stop();
-        //
-        //     firstListener.Unsubscribe();
-        //     secondListener.Unsubscribe();
         // }
+
+        private static async Task RunAsync(IServiceProvider serviceProvider)
+        {
+            var (firstClient, firstListener) = await SetupClientWithTextListener(serviceProvider);
+            var (secondClient, secondListener) = await SetupClientWithTextListener(serviceProvider);
+        
+            var firstClientMatrixRoom = await firstClient.CreateTrustedPrivateRoomAsync(new[]
+            {
+                secondClient.UserId
+            });
+        
+            var matrixRoom = await secondClient.JoinTrustedPrivateRoomAsync(firstClientMatrixRoom.Id);
+        
+            var spin = new SpinWait();
+            while (secondClient.JoinedRooms.Length == 0)
+                spin.SpinOnce();
+        
+            await firstClient.SendMessageAsync(firstClientMatrixRoom.Id, "Hello");
+            await secondClient.SendMessageAsync(secondClient.JoinedRooms[0].Id, ", ");
+        
+            await firstClient.SendMessageAsync(firstClientMatrixRoom.Id, "World");
+            await secondClient.SendMessageAsync(secondClient.JoinedRooms[0].Id, "!");
+        
+            Console.ReadLine();
+        
+            firstClient.Stop();
+            secondClient.Stop();
+        
+            firstListener.Unsubscribe();
+            secondListener.Unsubscribe();
+        }
 
         private static async Task<(MatrixClient, TextMessageListener)> SetupClientWithTextListener(IServiceProvider serviceProvider)
         {
+            SodiumCore.Init();
+            SodiumCore.Init();
+            SodiumCore.Init();
             var matrixClient = serviceProvider.GetRequiredService<MatrixClient>();
-            var cryptoService = serviceProvider.GetRequiredService<CryptoService>();
+            var cryptoService = serviceProvider.GetRequiredService<SignatureCryptoService>();
 
             var seed = Guid.NewGuid().ToString();
-            var keyPair = cryptoService.GenerateKeyPairFromSeed(seed);
+            var keyPair = SignatureCryptoService.GenerateEd25519KeyPair(seed);
 
             await matrixClient.StartAsync(keyPair); //Todo: generate once and then store seed?
-            
+
             var textMessageListener = new TextMessageListener(matrixClient.UserId, (listenerId, textMessageEvent) =>
             {
                 var (roomId, senderUserId, message) = textMessageEvent;
                 if (listenerId != senderUserId)
                     Console.WriteLine($"RoomId: {roomId} received message from {senderUserId}: {message}.");
             });
-            
+
             textMessageListener.ListenTo(matrixClient.MatrixEventNotifier);
 
             return (matrixClient, textMessageListener);
         }
-        
+
         // public override void OnNext(TextMessageEvent value)
         // {
         //     var (roomId, senderUserId, message) = value;
