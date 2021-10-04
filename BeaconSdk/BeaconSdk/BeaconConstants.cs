6e73e86abe1c136bd3ab63fd6428a0155b635fe3
iviich4eL <Mich4el@protonmail.com>
2021-10-04 15:15:13 +0300

    Create BeaconConstants.cs

---
+++ BeaconSdk/BeaconSdk/BeaconConstants.cs
@@ -0,0 +1,12 @@
+namespace BeaconSdk
+{
+    public static class BeaconConstants
+    {
+        public static readonly string[] KnownRelayServers =
+        {
+            "beacon-node-1.sky.papers.tech",
+            "beacon-node-0.papers.tech:8448",
+            "beacon-node-2.sky.papers.tech"
+        };
+    }
+}
