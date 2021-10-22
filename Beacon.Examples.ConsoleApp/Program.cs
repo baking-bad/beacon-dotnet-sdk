a5636d6cca272b0c6cc2b2d40f5d404efb75c37c
iviich4eL <Mich4el@protonmail.com>
2021-10-22 14:53:14 +0300

    Add Beacon

---
+++ Beacon.Examples.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Beacon.Examples.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
