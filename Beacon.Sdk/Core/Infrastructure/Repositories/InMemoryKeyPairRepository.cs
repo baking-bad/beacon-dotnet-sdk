db464a3b52ad49d9454d1fabc823bc5d773d85b7
iviich4eL <Mich4el@protonmail.com>
2021-11-15 14:30:41 +0300

    Create InMemoryKeyPairRepository.cs

---
+++ Beacon.Sdk/Core/Infrastructure/Repositories/InMemoryKeyPairRepository.cs
@@ -0,0 +1,54 @@
+namespace Beacon.Sdk.Core.Infrastructure.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using Cryptography.Libsodium;
+    using Domain.Interfaces;
+    using Domain.Interfaces.Data;
+    using Matrix.Sdk.Core.Utils;
+    using Sodium;
+
+    public class InMemoryKeyPairRepository : IKeyPairRepository
+    {
+        private static readonly Dictionary<HexString, KeyPair> ClientSessionKeyPairs = new();
+        private static readonly Dictionary<HexString, SessionKeyPair> ServerSessionKeyPairs = new();
+        private readonly ICryptographyService _cryptographyService;
+
+        public InMemoryKeyPairRepository(ICryptographyService cryptographyService)
+        {
+            _cryptographyService = cryptographyService;
+        }
+
+        public SessionKeyPair CreateOrReadServerSession(HexString clientPublicKey, KeyPair serverKeyPair) =>
+            ServerSessionKeyPairs.TryGetValue(clientPublicKey, out SessionKeyPair sessionKeyPair)
+                ? sessionKeyPair
+                : CreateServerSessionKeyPair(clientPublicKey, serverKeyPair);
+
+        private SessionKeyPair CreateServerSessionKeyPair(HexString clientPublicKey, KeyPair serverKeyPair)
+        {
+            SessionKeyPair sessionKeyPair =
+                _cryptographyService.CreateServerSessionKeyPair(clientPublicKey.ToByteArray(),
+                    serverKeyPair.PrivateKey);
+
+            ServerSessionKeyPairs[clientPublicKey] = sessionKeyPair;
+
+            return sessionKeyPair;
+        }
+        
+        private KeyPair? _keyPair;
+        public KeyPair KeyPair
+        {
+            get
+            {
+                // Todo: add storage
+                if (_keyPair != null)
+                    return _keyPair;
+
+                var seed = Guid.NewGuid().ToString();
+                _keyPair = _cryptographyService.GenerateEd25519KeyPair(seed);
+
+                return _keyPair;   
+            }
+        }
+    }
+}
